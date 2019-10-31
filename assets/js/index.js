db403505d4f4b90ff7a6870980de3b051733ea6f
Dendevpro <mauro.facciolla@gmail.com>
2019-10-31 15:14:14 -0700

    Finished Web Dev Mockups

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,11 +19,9 @@ $(window).scroll(function () {
 // PORTFOLIO PAGE
 
 //Get the button:
-mybutton = document.getElementById("toTopBtn");
-
-// When the user scrolls down 1080px from the top of the document, show "scroll-to-top" the button
+var mybutton = document.getElementById("toTopBtn");
+// // When the user scrolls down 1080px from the top of the document, show "scroll-to-top" button
 window.onscroll = function () { scrollFunction() };
-
 function scrollFunction() {
     if (document.body.scrollTop > 1080 || document.documentElement.scrollTop > 1080) {
         mybutton.style.display = "block";
@@ -31,7 +29,6 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
